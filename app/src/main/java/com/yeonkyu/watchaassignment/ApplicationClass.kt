d620af44460a1705599f2c9af0788af26825375d
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-04-05 16:37:46 +0900

    Add dependency injection module

--- app/src/main/java/com/yeonkyu/watchaassignment/ApplicationClass.kt
+++ app/src/main/java/com/yeonkyu/watchaassignment/ApplicationClass.kt
@@ -1,6 +1,9 @@
 package com.yeonkyu.watchaassignment
 
 import android.app.Application
+import com.yeonkyu.watchaassignment.di.networkModule
+import com.yeonkyu.watchaassignment.di.repositoryModule
+import com.yeonkyu.watchaassignment.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,6 +16,8 @@ class ApplicationClass : Application(){
         startKoin {
             androidLogger()
             androidContext(this@ApplicationClass)
+            modules(viewModelModule, networkModule, repositoryModule)
+
         }
     }
 
--- app/src/main/java/com/yeonkyu/watchaassignment/di/RepositoryModule.kt
+++ app/src/main/java/com/yeonkyu/watchaassignment/di/RepositoryModule.kt
@@ -2,6 +2,6 @@ package com.yeonkyu.watchaassignment.di
 
 import org.koin.dsl.module
 
-val RepositoryModule = module {
+val repositoryModule = module {
 
 }
