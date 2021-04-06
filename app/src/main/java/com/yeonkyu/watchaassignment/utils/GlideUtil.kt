9066f1880cfed9460221ddc3b9338c963c7d3ebd
yeon-kyu <45001093+yeon-kyu@users.noreply.github.com>
2021-04-06 17:41:29 +0900

    Add GlideUtil

---
+++ app/src/main/java/com/yeonkyu/watchaassignment/utils/GlideUtil.kt
@@ -0,0 +1,24 @@
+package com.yeonkyu.watchaassignment.utils
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.yeonkyu.watchaassignment.R
+
+class GlideUtil {
+    companion object{
+
+        fun displayImageFromUrl(
+                context: Context,
+                url: String,
+                imageView: ImageView
+        ) {
+            Glide.with(context)
+                    .load(url)
+                    .placeholder(R.drawable.img_placeholder)
+                    .into(imageView)
+
+        }
+
+    }
+}
